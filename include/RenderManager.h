fd94bc6ee2fe290a50745f5c953af1475ece6c7b
thibault-manse <thibault.manse@laplateforme.io>
2025-12-19 09:25:04 +0100

    feat : add number and color for each tiles (one number = one same color)

--- include/RenderManager.h
+++ include/RenderManager.h
@@ -1,13 +1,19 @@
 #pragma once
 #include <SDL.h>
+#include <SDL2/SDL_ttf.h>
 #include "Grid.h"
 
 class RenderManager
 {
 public:
     RenderManager(SDL_Renderer *renderer);
+    ~RenderManager();
+
     void draw(const Grid &grid);
 
 private:
     SDL_Renderer *renderer;
+    TTF_Font *font;
+
+    SDL_Color getTileColor(int value);
 };
--- src/rendermanager.cpp
+++ src/rendermanager.cpp
@@ -1,6 +1,19 @@
 #include "RenderManager.h"
+#include <string>
 
-RenderManager::RenderManager(SDL_Renderer *r) : renderer(r) {}
+RenderManager::RenderManager(SDL_Renderer *r) : renderer(r)
+{
+    font = TTF_OpenFont("asset/font/arial.ttf", 24);
+    if (!font)
+    {
+        SDL_Log("Failed to load font: %s", TTF_GetError());
+    }
+}
+
+RenderManager::~RenderManager()
+{
+    TTF_CloseFont(font);
+}
 
 void RenderManager::draw(const Grid &grid)
 {
@@ -11,14 +24,68 @@ void RenderManager::draw(const Grid &grid)
     {
         for (int j = 0; j < Grid::SIZE; j++)
         {
-            SDL_Rect rect = {j * 110 + 10, i * 110 + 10, 100, 100};
-            if (grid.cells[i][j] == 0)
-                SDL_SetRenderDrawColor(renderer, 200, 200, 200, 255);
-            else
-                SDL_SetRenderDrawColor(renderer, 255, 180, 100, 255);
+            int value = grid.cells[i][j];
 
-            SDL_RenderFillRect(renderer, &rect);
+            SDL_Rect tile = {j * 110 + 10, i * 110 + 10, 100, 100};
+            SDL_Color color = getTileColor(value);
+
+            SDL_SetRenderDrawColor(renderer, color.r, color.g, color.b, 255);
+            SDL_RenderFillRect(renderer, &tile);
+
+            if (value != 0 && font)
+            {
+                SDL_Color textColor = (value <= 4)
+                                          ? SDL_Color{119, 110, 101, 255}
+                                          : SDL_Color{249, 246, 242, 255};
+
+                std::string text = std::to_string(value);
+
+                SDL_Surface *surface = TTF_RenderText_Blended(font, text.c_str(), textColor);
+
+                SDL_Texture *texture = SDL_CreateTextureFromSurface(renderer, surface);
+
+                SDL_Rect dst;
+                dst.w = surface->w;
+                dst.h = surface->h;
+                dst.x = tile.x + (tile.w - dst.w) / 2;
+                dst.y = tile.y + (tile.h - dst.h) / 2;
+
+                SDL_FreeSurface(surface);
+                SDL_RenderCopy(renderer, texture, nullptr, &dst);
+                SDL_DestroyTexture(texture);
+            }
         }
     }
     SDL_RenderPresent(renderer);
+}
+
+SDL_Color RenderManager::getTileColor(int value)
+{
+    switch (value)
+    {
+    case 2:
+        return {238, 228, 218, 255};
+    case 4:
+        return {237, 224, 200, 255};
+    case 8:
+        return {242, 177, 121, 255};
+    case 16:
+        return {245, 149, 99, 255};
+    case 32:
+        return {246, 124, 95, 255};
+    case 64:
+        return {246, 94, 59, 255};
+    case 128:
+        return {237, 207, 114, 255};
+    case 256:
+        return {237, 204, 97, 255};
+    case 512:
+        return {237, 200, 80, 255};
+    case 1024:
+        return {237, 197, 63, 255};
+    case 2048:
+        return {237, 194, 46, 255};
+    default:
+        return {205, 193, 180, 255};
+    }
 }
